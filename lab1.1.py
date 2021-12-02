a3bac307b4433ebcb50b2e0412d7a106b417cd36
nem1sha <91321132+nem1sha@users.noreply.github.com>
2021-12-02 20:21:44 +0300

    Update lab1.1.py

--- lab1.1.py
+++ lab1.1.py
@@ -5,25 +5,24 @@ pokemon_full = file.read()
 
 print('1. Общее количество символов:', len(pokemon_full))
 
-count = 0
+amount = 0
 for i in pokemon_full:
     if i.isalnum():
-        count += 1
-print('Общее количество символов без пробелов и знаков препинания:', count)
+        amount += 1
+print('2. Общее количество символов без пробелов и знаков препинания:', amount)
 
 pokemon_full_list = json.loads(pokemon_full)
 max_length = 0
 name_of_pokemon = ''
 max_number = 0
-for char in pokemon_full_list:
-    if len(char["description"]) > max_length:
-        max_length = len(char["description"])
-        name_of_pokemon = char["name"]
-    for ability in char['abilities']:
-        if len(ability.split()) > max_number:
-            max_number = len(ability.split())
-            longest_ability = ability
-
+if __name__ == '__main__':
+    for char in pokemon_full_list:
+        if len(char["description"]) > max_length:
+            max_length = len(char["description"])
+            name_of_pokemon = char["name"]
+        for ability in char['abilities']:
+            if len(ability.split()) > max_number:
+                max_number = len(ability.split())
+                longest_ability = ability
 print('3. Покемон с самым длинным описанием:', name_of_pokemon)
-
 print('4. Умение с самым большим количеством слов: ', longest_ability)
