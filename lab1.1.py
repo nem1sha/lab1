1d2e1adb3781130804d27a5270d116635a3fda13
nem1sha <91321132+nem1sha@users.noreply.github.com>
2021-12-07 01:15:35 +0300

    Update lab1.1.py

--- lab1.1.py
+++ lab1.1.py
@@ -1,28 +1,46 @@
 import json
 
-file = open('pokemon_full.json')
-pokemon_full = file.read()
+def read_file():
+    file = open('pokemon_full.json')
+    a = file.read()
+    b = json.loads(a)
+    return a, b
 
-print('1. Общее количество символов:', len(pokemon_full))
+def count_symbols(a):
+    return len(a)
 
-amount = 0
-for i in pokemon_full:
-    if i.isalnum():
-        amount += 1
-print('2. Общее количество символов без пробелов и знаков препинания:', amount)
+def count_no_signs(a):
+    count = 0
+    for i in a:
+        if i.isalnum():
+            count += 1
+    return count
 
-pokemon_full_list = json.loads(pokemon_full)
-max_length = 0
-name_of_pokemon = ''
-max_number = 0
-if __name__ == '__main__':
-    for char in pokemon_full_list:
+def search_max_dscrp(b):
+    max_length = 0
+    name_of_pokemon = ''
+    for char in b:
         if len(char["description"]) > max_length:
             max_length = len(char["description"])
             name_of_pokemon = char["name"]
-        for ability in char['abilities']:
-            if len(ability.split()) > max_number:
-                max_number = len(ability.split())
-                longest_ability = ability
-print('3. Покемон с самым длинным описанием:', name_of_pokemon)
-print('4. Умение с самым большим количеством слов: ', longest_ability)
+    return name_of_pokemon
+
+def search_longest_ability(b):
+    max_number = 0
+    ability = ''
+    for skills in b:
+        for skill in skills['abilities']:
+            if len(skill.split()) > max_number:
+                max_number = len(skill.split())
+                ability = skill
+    return ability
+
+def print_results(a, b):
+    print('1. Общее количество символов:', count_symbols(a))
+    print('2. Общее количество символов без пробелов и знаков препинания:', count_no_signs(a))
+    print('3. Покемон с самым длинным описанием:', search_max_dscrp(b))
+    print('4. Умение с самым большим количеством слов: ', search_longest_ability(b))
+
+if __name__ == '__main__':
+    pokemon_full, pokemon_full_list = read_file()
+    print_results(pokemon_full, pokemon_full_list)
